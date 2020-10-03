bb317be9267432a6401c7bf08a58cc4601a50936
farescalms <fares@calms.eu>
2020-10-03 19:31:48 +0200

    juhu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kalkulator';
+  public stevke = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  public output = '';
+  public savedoutput: number;
+  operacija: string;
+
+  public addstevke(stevka) {
+    this.output += stevka;
+  }
+
+  public saveoutput(operacija) {
+    if (this.output === '') {
+      return;
+    }
+    if (this.savedoutput) {
+      this.equals();
+    }
+    else {
+      this.savedoutput = Number(this.output);
+    }
+    this.output = '';
+    this.operacija = operacija;
+  }
+
+  public equals() {
+    const numberoutput = Number(this.output);
+    switch (this.operacija) {
+      case '+':
+        this.savedoutput += numberoutput;
+        break;
+      case '-':
+        this.savedoutput -= numberoutput;
+        break;
+      case '*':
+        this.savedoutput *= numberoutput;
+        break;
+      case '/':
+        this.savedoutput /= numberoutput;
+        break;
+
+    }
+  }
 }
